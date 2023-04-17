9eba3486c4a26b0e8a2d0283efb7d163e3ed1ab4
s.erfan.m <104711007+s-erfanm@users.noreply.github.com>
2023-04-17 18:26:43 +0330

    first_project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  int first_num, second_num;
+  cout<<"enter your first number :";
+  cin>>first_num;
+  cout<<"enter your second number :";
+  cin>>second_num;
+  cout<<"\nsum is :"<<first_num + second_num;
+  cout<<"\nsubtraction is :"<<first_num - second_num;
+  cout<<"\nproduct is :"<<first_num * second_num;
+  cout<<"\nquotient is :"<<first_num % second_num;
+
+  return 0;
+}
